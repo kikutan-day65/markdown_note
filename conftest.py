a9c915f733285e7dd6b049430cea66c9f18d88e3
haruto.ubuntu <haruto-cs@outlook.com>
2025-07-05 17:34:02 +0900

    add fixtures for home app  models test

--- conftest.py
+++ conftest.py
@@ -1,5 +1,8 @@
 import pytest
 from django.contrib.auth import get_user_model
+from django.core.files.uploadedfile import SimpleUploadedFile
+
+from home.models import Article
 
 
 @pytest.fixture
@@ -34,3 +37,20 @@ def taken_user(db):
 def authenticated_client(client, test_user):
     client.login(username="testuser", password="testpassword123")
     return client
+
+
+@pytest.fixture
+def test_article(test_user):
+    article = Article.objects.create(
+        title="test_title", content="test_content", user=test_user
+    )
+
+    return article
+
+
+@pytest.fixture
+def test_image():
+    test_image = SimpleUploadedFile(
+        name="test_image.jpg", content=b"test-image", content_type="image/jpeg"
+    )
+    return test_image
