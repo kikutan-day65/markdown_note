1019ee434c42a3e612e63d6b0b8a10c80c5e350a
haruto.ubuntu <haruto-cs@outlook.com>
2025-07-11 22:29:46 +0900

    enable debug tool bar only for development

--- markdown_note/urls.py
+++ markdown_note/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("home.urls")),
     path("user/", include("user.urls")),
-] + debug_toolbar_urls()
+]
 
 if settings.DEBUG:
+    urlpatterns += debug_toolbar_urls()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
