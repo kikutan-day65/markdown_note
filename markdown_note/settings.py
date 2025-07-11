dffe18603899eb2b493b5e6c2ec40281a43891c0
haruto.ubuntu <haruto-cs@outlook.com>
2025-07-11 22:24:18 +0900

    set allowed hosts according to debug mode

--- markdown_note/settings.py
+++ markdown_note/settings.py
@@ -32,7 +32,11 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+if DEBUG:
+    ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+else:
+    ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+
 
 # Application definition
 
