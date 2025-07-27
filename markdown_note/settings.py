fdae4d812257daf5fcd69a13f77bfe5645877373
haruto.ubuntu <haruto-cs@outlook.com>
2025-07-27 22:48:57 +0900

    remove useless line

--- markdown_note/settings.py
+++ markdown_note/settings.py
@@ -156,12 +156,6 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
