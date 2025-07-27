6d75f0eb82e5f97be9d3b95643f46b72b09ca691
haruto.ubuntu <haruto-cs@outlook.com>
2025-07-27 23:44:29 +0900

    add login required message as constant in forbid anonymous decorator

--- home/decorators.py
+++ home/decorators.py
@@ -2,14 +2,14 @@ from functools import wraps
 
 from django.http import JsonResponse
 
+from home.messages import LOGIN_REQUIRED
+
 
 def forbid_anonymous(view_func):
     @wraps(view_func)
     def wrapper(request, *args, **kwargs):
         if not request.user.is_authenticated:
-            return JsonResponse(
-                {"success": False, "error": "Forbidden: Login required"}, status=403
-            )
+            return JsonResponse({"success": False, "error": LOGIN_REQUIRED}, status=403)
         return view_func(request, *args, **kwargs)
 
     return wrapper
